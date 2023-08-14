cef782ce4b7ae12b90b7dac2aeab63fdd2942f84
Abdelbari Messah <amessah@e3r1p11.1337.ma>
2023-08-14 21:41:24 +0100

    push the recent version :::)

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -3,38 +3,29 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+
 int main()
 {
    try {
-		Bureaucrat	_Bureaucrat("Office", 137);
+		Bureaucrat	_Bureaucrat("f1", 137);
 		AForm *F1 = new  ShrubberyCreationForm(_Bureaucrat.getName()); //causing leaks !!!!! must delete
 		_Bureaucrat.ExecuteForm(*F1);
 		
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
-	//--------------------------------
    try {
-		Bureaucrat	_Bureaucrat("Office_987", 45);
+		Bureaucrat	_Bureaucrat("f2", 45);
 		AForm *F2 = new  RobotomyRequestForm(_Bureaucrat.getName()); //causing leaks !!!!! must delete
+		F2->beSigned(_Bureaucrat);
 		_Bureaucrat.ExecuteForm(*F2);
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
-	//--------------------------------
    try {
-		Bureaucrat	_Bureaucrat("Office_000", 5);
+		Bureaucrat	_Bureaucrat("f3", 5);
 		AForm *F3 = new  PresidentialPardonForm(_Bureaucrat.getName()); //causing leaks !!!!! must delete
-		_Bureaucrat.ExecuteForm(*F3);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	
-	//--------------------------------
-	std::cout << "Shouldn't Work !" << std::endl;
-   try {
-		Bureaucrat	_Bureaucrat("Office_4454", 151);
-		AForm *F3 = new  ShrubberyCreationForm(_Bureaucrat.getName()); //causing leaks !!!!! must delete
+		F3->beSigned(_Bureaucrat);
 		_Bureaucrat.ExecuteForm(*F3);
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
